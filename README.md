629af9c1e20b8ca243e285fdca1c081dd0a2dcf9
Tristan Hanson <thanson1998@live.com>
2022-10-10 17:16:52 -0500

    functioning code commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+    #undefined
+    ## Description
+    test
+    ## Motivation
+    test
+    ## What I wanted to solve
+    test
+    ## What I learned
+    test
+    ## What makes my project stand out
+    test
+
+    ## 
--- index.js
+++ index.js
@@ -1,2 +1,55 @@
 const fs = require("fs")
 const inquirer = require("inquirer")
+
+inquirer.prompt([
+    {
+        name: "title",
+        type: "input",
+        Message: "What is the title of your project?"
+    },
+    {
+        name: "description",
+        type: "input",
+        message: "Give a brief description of your project?"
+    },
+    {
+        name: "motivation",
+        type: "input",
+        message: "What was the motivation for this project?"
+    },
+    {
+        name: "problem",
+        type: "input",
+        message: "What problems does this solve?"
+    },
+    {
+        name: "learn",
+        type: "input",
+        message: "What did you learn from this project?"
+    },
+    {
+        name: "unique",
+        type: "input",
+        message: "What makes your project stand out from others?"
+    },
+])
+.then((data) =>{
+    fs.writeFile('README.md', generateMarkdown(data), (err) =>
+    err ? console.log(err) : console.log('Generating Markdown')
+    );6
+});
+
+function generateMarkdown(data){
+    return `
+    #${data.title}
+    ## Description
+    ${data.description}
+    ## Motivation
+    ${data.motivation}
+    ## What I wanted to solve
+    ${data.problem}
+    ## What I learned
+    ${data.learn}
+    ## What makes my project stand out
+    ${data.unique}`
+}
